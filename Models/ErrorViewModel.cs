c04484a4a31dae2fc7cfd1bbe484e4c417d34aca
Azlan Abas <mohdazlan.abas.com>
2022-04-28 11:51:29 +0800

    Initial Update

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cs_Authn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+   {
+       options.LoginPath = "/login";
+       options.AccessDeniedPath = "/denied";
+       options.Events = new CookieAuthenticationEvents()
+       {
+
+           OnSigningIn = async context =>
+           {
+               var principal = context.Principal;
+               await Task.CompletedTask;
+           },
+           OnSignedIn = async context =>
+           {
+               await Task.CompletedTask;
+           },
+           OnValidatePrincipal = async context =>
+           {
+               await Task.CompletedTask;
+           }
+
+       };
+   });
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
